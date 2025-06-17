78999c731bc58a4119857eb81f44da67f3adc58f
Sravan Goud <tsravangoud164@gmail.com>
2025-06-17 22:06:52 +0530

    initial setup

---
+++ FoodX/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FoodX
+//
+//  Created by Sravan Goud on 17/06/25.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+
+}
